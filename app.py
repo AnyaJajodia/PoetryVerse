9e210b33df5a15f4108511f804c0292c21eb5fa8
Anya Jajodia <85209106+AnyaJajodia@users.noreply.github.com>
2025-03-30 15:35:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import os
 from werkzeug.utils import secure_filename
 
-from dotenv import load_dotenv
-load_dotenv()
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-# app.config['SECRET_KEY'] = 'secret_key'  # Use a secure key in production
+app.config['SECRET_KEY'] = 'secret_key'  # Use a secure key in production
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = "Please log in to access this page."
@@ -653,3 +647,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
